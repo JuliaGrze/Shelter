95823414d50f1e039a0c11c0c452a327083a3893
JuliaGrze <julia.grzesiewicz@op.pl>
2025-10-05 14:12:16 +0200

    Added to Angular display details of the selected animal

--- frontend/Shelter/src/app/app.routes.ts
+++ frontend/Shelter/src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { AnimalList } from './features/animals/animal-list/animal-list';
+import { AnimalDetails } from './features/animals/animal-details/animal-details';
 
 export const routes: Routes = [
-    {path: '', component: AnimalList},
+    {path: 'animals', component: AnimalList},
+    {path: 'animals/:id', component: AnimalDetails},
+    {path: '**', redirectTo: 'animals'}
+
 ];

cbe1fbdb7d1618ae440174996d7791242f5a5198
JuliaGrze <julia.grzesiewicz@op.pl>
2025-10-05 22:40:58 +0200

    Added animals pagination and sorting to frontend

--- frontend/Shelter/src/app/core/services/animal.service.ts
+++ frontend/Shelter/src/app/core/services/animal.service.ts
@@ -1,8 +1,10 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { enviroment} from '../../../environments/environment'
 import { Observable } from 'rxjs';
 import { AnimalDto, CreateAnimalDto } from '../models/animal';
+import { AnimalQuery } from '../models/animal-query';
+import { PagedResult } from '../models/paged-result';
 
 @Injectable({
   providedIn: 'root'
@@ -11,9 +13,20 @@ export class AnimalService {
   private http = inject(HttpClient)
   private base = `${enviroment.apiUrl}/animals`
 
-  // GET /api/animals
+  // GET /api/animals/all
   getAllAnimals() : Observable<AnimalDto[]>{
     return this.http.get<AnimalDto[]>(this.base)
+  }
+
+  // GET: api/animals?sortBy=name&sortDir=asc&page=0&size=20
+  getAnimalsPaged(q: AnimalQuery = {}): Observable<PagedResult<AnimalDto>> {
+    let params = new HttpParams();
+    if (q.sortBy)  params = params.set('sortBy', q.sortBy);
+    if (q.sortDir) params = params.set('sortDir', q.sortDir);
+    if (q.page !== undefined) params = params.set('page', q.page);
+    if (q.size !== undefined) params = params.set('size', q.size);
+
+    return this.http.get<PagedResult<AnimalDto>>(this.base, { params });
   }
 
   // GET /api/animals/{id}

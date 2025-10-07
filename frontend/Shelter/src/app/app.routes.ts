cdd5045520aeefa6a84c14938113a0448a7884ea
JuliaGrze <julia.grzesiewicz@op.pl>
2025-10-08 00:03:56 +0200

    Added front-end login/registration integration with JWT backend

--- frontend/Shelter/src/app/app.routes.ts
+++ frontend/Shelter/src/app/app.routes.ts
@@ -1,10 +1,17 @@
 import { Routes } from '@angular/router';
 import { AnimalList } from './features/animals/animal-list/animal-list';
 import { AnimalDetails } from './features/animals/animal-details/animal-details';
+import { Login } from './features/auth/login/login';
+import { Register } from './features/auth/register/register';
 
 export const routes: Routes = [
     {path: 'animals', component: AnimalList},
     {path: 'animals/:id', component: AnimalDetails},
+
+    //AUTH - login & register
+    {path: 'login', component: Login},
+    {path: 'register', component: Register },
+
     {path: '**', redirectTo: 'animals'}
 
 ];
